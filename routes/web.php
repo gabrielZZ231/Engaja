b7756f592c33c7c3ebad6a40bda41b2ae3bb57a7
Douglas Filipe <batista.douglasfs@gmail.com>
2025-09-05 15:35:58 -0300

    feat: tela para o participante se cadastrar, inscrição e presença realizados automaticamente no evento

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AtividadeController;
 use App\Http\Controllers\InscricaoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,15 +29,18 @@ Route::middleware(['auth', 'role:administrador'])->group(function () {
     Route::get('/eventos/{evento}/inscricoes/preview', [InscricaoController::class, 'preview'])->name('inscricoes.preview');
     Route::post('/eventos/{evento}/inscricoes/preview/save', [InscricaoController::class, 'savePage'])->name('inscricoes.preview.save');
     Route::post('/eventos/{evento}/inscricoes/confirmar', [InscricaoController::class, 'confirmar'])->name('inscricoes.confirmar');
-    Route::get('/eventos/{evento}/inscritos', [\App\Http\Controllers\InscricaoController::class, 'inscritos'])->name('inscricoes.inscritos');
+    Route::get('/eventos/{evento}/inscritos', [InscricaoController::class, 'inscritos'])->name('inscricoes.inscritos');
 });
 
 Route::middleware(['auth', 'role:administrador|participante'])->group(function () {
-    Route::resource('eventos', \App\Http\Controllers\EventoController::class);
+    Route::resource('eventos', EventoController::class);
 });
 
 Route::resource('eventos.atividades', AtividadeController::class)
     ->parameters(['atividades' => 'atividade'])
     ->shallow();
+
+Route::get('/eventos/{evento}/cadastro-e-inscricao', [EventoController::class, 'cadastro_inscricao'])->name('evento.cadastro_inscricao');
+Route::post('/eventos/cadastro-e-inscricao/store', [EventoController::class, 'store_cadastro_inscricao'])->name('evento.store_cadastro_inscricao');
 
 require __DIR__ . '/auth.php';
